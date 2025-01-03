a3aed0b45686563fbcdf16cf3e695c20614f8c32
VKostovsky <SpasiboMiryZaZemfiry@gmail.com>
2025-01-03 18:53:06 +0700

    lab4 fix gridmake (?)

--- lab4/double_lab4.for
+++ lab4/double_lab4.for
@@ -1,99 +1,103 @@
-PROGRAM main
-    IMPLICIT NONE
-    REAL*8 h, a, b
-    INTEGER N, max, i
-    COMMON /gridparam/ h, a, b, N
-    REAL*8 grid(100000000)
+      PROGRAM MAIN
+      IMPLICIT NONE
+      DOUBLE PRECISION H, A, B
+      INTEGER N, MAX, I
+      COMMON /GRIDPARAM/ H, A, B, N
+      DOUBLE PRECISION GRID(1000000)
 
-    PRINT *, '1) trapezia'
-    PRINT *, '3) gauss-4'
-    PRINT *, '4) vihod'
-    
-    READ *, i
-    IF (i .EQ. 4) GOTO 100
+      PRINT *, '1) TRAPEZIA'
+      PRINT *, '3) GAUSS-4'
+      PRINT *, '4) VIHOD'
+      
+      READ *, I
+      IF (I .EQ. 4) GOTO 100
 
-    PRINT *, 'Enter segment and [a,b]'
-    READ *, max, a, b
+      PRINT *, 'ENTER SEGMENT AND [A,B]'
+      READ *, MAX, A, B
 
-    N = 2
-    GOTO (10, 30) i
- 10 CALL trapeze(grid, max)
-    GOTO 100
- 30 CALL gauss4(grid, max)
-    GOTO 100
+      N = 1  ! Начинаем с 2^0 = 1
+      IF (I .EQ. 1) THEN
+          CALL TRAPEZE(GRID, MAX)
+      ELSE IF (I .EQ. 3) THEN
+          CALL GAUSS4(GRID, MAX)
+      ELSE
+          PRINT *, 'INVALID OPTION'
+      END IF
 
-100 END
+  100 END
 
-    SUBROUTINE Gridmake(grid)
-    IMPLICIT NONE
-    REAL*8 h, a, b
-    INTEGER N, i
-    COMMON /gridparam/ h, a, b, N
-    REAL*8 grid(N)
+      SUBROUTINE GRIDMAKE(GRID)
+      IMPLICIT NONE
+      DOUBLE PRECISION H, A, B
+      INTEGER N, I
+      COMMON /GRIDPARAM/ H, A, B, N
+      DOUBLE PRECISION GRID(N)
 
-    h = (b - a) / (N - 1)
-    grid(1) = a
-    DO i = 2, N
-        grid(i) = a + h * (i - 1)
-    END DO
-    END
+      H = (B - A) / DBLE(N - 1)
+      GRID(1) = A
+      DO 10 I = 2, N
+          GRID(I) = A + H * DBLE(I - 1)
+   10 CONTINUE
+      END
 
-    SUBROUTINE trapeze(grid, max)
-    IMPLICIT NONE
-    REAL*8 h, a, b
-    INTEGER N, max, i
-    COMMON /gridparam/ h, a, b, N
-    REAL*8 grid(N)
-    REAL*8 ans, fun
-    EXTERNAL fun
+      SUBROUTINE TRAPEZE(GRID, MAX)
+      IMPLICIT NONE
+      DOUBLE PRECISION H, A, B
+      INTEGER N, MAX, I
+      COMMON /GRIDPARAM/ H, A, B, N
+      DOUBLE PRECISION GRID(N)
+      DOUBLE PRECISION ANS, FUN
+      EXTERNAL FUN
 
-    DO WHILE (N .LE. max + 1)
-        CALL Gridmake(grid)
-        ans = h * (fun(grid(1)) + fun(grid(N))) / 2.0
-        DO i = 2, N - 1
-            ans = ans + h * fun(grid(i))
-        END DO
-        PRINT *, 'Trapezoidal rule result for N =', N, ':', ans
-        N = 2 * N - 1
-    END DO
-    END
+   20 IF (N .LE. MAX) THEN
+          CALL GRIDMAKE(GRID)
+          ANS = H * (FUN(GRID(1)) + FUN(GRID(N))) / 2.0D0
+          DO 30 I = 2, N - 1
+              ANS = ANS + H * FUN(GRID(I))
+   30     CONTINUE
+          PRINT *, 'TRAPEZOIDAL RULE RESULT FOR N =', N, ':', ANS
+          N = 2 * N  ! Удваиваем N
+          GOTO 20
+      END IF
+      END
 
-    SUBROUTINE gauss4(grid, max)
-    IMPLICIT NONE
-    REAL*8 h, a, b
-    INTEGER N, max, i
-    COMMON /gridparam/ h, a, b, N
-    REAL*8 grid(N)
-    REAL*8 ans, x1, x2, x3, x4, w1, w2, w3, w4, mid, fun
-    EXTERNAL fun
+      SUBROUTINE GAUSS4(GRID, MAX)
+      IMPLICIT NONE
+      DOUBLE PRECISION H, A, B
+      INTEGER N, MAX, I
+      COMMON /GRIDPARAM/ H, A, B, N
+      DOUBLE PRECISION GRID(N)
+      DOUBLE PRECISION ANS, X1, X2, X3, X4, W1, W2, W3, W4, MID, FUN
+      EXTERNAL FUN
 
-    w1 = 0.3478548451
-    w2 = 0.6521451549
-    w3 = 0.6521451549
-    w4 = 0.3478548451
-    x1 = -0.8611363116
-    x2 = -0.3399810436
-    x3 = 0.3399810436
-    x4 = 0.8611363116
+      W1 = 0.3478548451D0
+      W2 = 0.6521451549D0
+      W3 = 0.6521451549D0
+      W4 = 0.3478548451D0
+      X1 = -0.8611363116D0
+      X2 = -0.3399810436D0
+      X3 = 0.3399810436D0
+      X4 = 0.8611363116D0
 
-    DO WHILE (N .LE. max + 1)
-        CALL Gridmake(grid)
-        ans = 0.0
-        DO i = 1, N - 1
-            h = (grid(i + 1) - grid(i)) / 2.0
-            mid = (grid(i + 1) + grid(i)) / 2.0
-            ans = ans + h * (w1 * fun(mid + h * x1) + 
-   &                         w2 * fun(mid + h * x2) +
-   &                         w3 * fun(mid + h * x3) + 
-   &                         w4 * fun(mid + h * x4))
-        END DO
-        PRINT *, 'Gauss-4 rule result for N =', N, ':', ans
-        N = 2 * N - 1
-    END DO
-    END
+   40 IF (N .LE. MAX) THEN
+          CALL GRIDMAKE(GRID)
+          ANS = 0.0D0
+          DO 50 I = 1, N - 1
+              H = (GRID(I + 1) - GRID(I)) / 2.0D0
+              MID = (GRID(I + 1) + GRID(I)) / 2.0D0
+              ANS = ANS + H * (W1 * FUN(MID + H * X1) + 
+     &                         W2 * FUN(MID + H * X2) +
+     &                         W3 * FUN(MID + H * X3) + 
+     &                         W4 * FUN(MID + H * X4))
+   50     CONTINUE
+          PRINT *, 'GAUSS-4 RULE RESULT FOR N =', N, ':', ANS
+          N = 2 * N  ! Удваиваем N
+          GOTO 40
+      END IF
+      END
 
-    REAL*8 FUNCTION fun(x)
-    IMPLICIT NONE
-    REAL*8 x
-    fun = 1000.0 * SIN(5.0 * x)
-    END
+      DOUBLE PRECISION FUNCTION FUN(X)
+      IMPLICIT NONE
+      DOUBLE PRECISION X
+      FUN = 1000.0D0 * SIN(5.0D0 * X)
+      END
