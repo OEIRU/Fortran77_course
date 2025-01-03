3e42f1f5373102fb01d0f39e81eb95a5ca38ca15
VKostovsky <SpasiboMiryZaZemfiry@gmail.com>
2025-01-03 17:56:08 +0700

    lab2 fix build table

--- lab2/lab2.for
+++ lab2/lab2.for
@@ -1,245 +1,225 @@
-    PROGRAM MAIN                                        
-        IMPLICIT NONE
-        COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP  ! Глобальные переменные
-        REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP            
-        
-        CALL READ_PARAMS()                                         ! Вызов функций
-        
-        CALL CREATE_TABLE()
-        
-    END
+      PROGRAM MAIN
+          IMPLICIT NONE
+          COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
+          REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
 
+          CALL READ_PARAMS()
+          CALL CREATE_TABLE()
 
-    SUBROUTINE READ_PARAMS()
-        IMPLICIT NONE
-        COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
-        REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
-        
-        OPEN(10, FILE='params.txt', ERR=100)            ! Открытие файла
-        READ(10, *, ERR=101) X_MIN, X_MAX
-        READ(10, *, ERR=101) Y_MIN, Y_MAX
-        READ(10, *, ERR=101) X_STEP, Y_STEP
-        CLOSE(10)
-        
-        IF(X_MAX.LE.X_MIN.OR.Y_MAX.LE.Y_MIN) THEN       ! Если начальное больше конечного - ошибка 
-            GOTO 102
-        ENDIF
-        
-        IF(X_STEP.LE.0.0.OR.Y_STEP.LE.0.0) THEN         ! шаг <= 0 - ошибка
-            GOTO 103
-        ENDIF
-        
-        GOTO 109
-        
-    100       PAUSE 'Open params file error!'
-        STOP
-        
-    101       PAUSE 'Read params file error!'
-        CLOSE(10)
-        STOP
-        
-    102       PAUSE 'Incorrect data, max must be greater than min!'
-        STOP
-        
-    103       PAUSE 'Incorrect data, step must be greater than 0!'
-        STOP    
-        
-    109       CONTINUE              
-    END
+      END
 
+      SUBROUTINE READ_PARAMS()
+          IMPLICIT NONE
+          COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
+          REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
 
-    REAL FUNCTION TO_RAD(ANGLE)                    ! Функция перевод градус в радиан
-        IMPLICIT NONE
-        REAL ANGLE
-        TO_RAD = ANGLE / 180 * 3.1415926
-    END
+          OPEN(10, FILE='params.txt', ERR=100)
+          READ(10, *, ERR=101) X_MIN, X_MAX
+          READ(10, *, ERR=101) Y_MIN, Y_MAX
+          READ(10, *, ERR=101) X_STEP, Y_STEP
+          CLOSE(10)
 
+          IF (X_MAX .LE. X_MIN .OR. Y_MAX .LE. Y_MIN) THEN
+              GOTO 102
+          END IF
 
-    INTEGER FUNCTION IS_INVISIBLE(FIRST, SECOND)
-        IMPLICIT NONE
-        REAL FIRST, SECOND
-        IS_INVISIBLE = 0        
-        IF (ABS(FIRST - SECOND) .LT. 1.0E-6) THEN
-            IS_INVISIBLE = 1                       
-        ENDIF
-        RETURN
-    END
+          IF (X_STEP .LE. 0.0 .OR. Y_STEP .LE. 0.0) THEN
+              GOTO 103
+          END IF
 
+          GOTO 109
 
-    REAL FUNCTION ARCCOS(X, Y)
-        IMPLICIT NONE
-        REAL X, Y, SUM
-        SUM = X + Y
-        IF (SUM.LT. -1.0001 .OR. SUM.GT. 1.0001) THEN
-            ARCCOS = 0.0
-        ELSE
-            ARCCOS = ACOS(SUM)
-        ENDIF
-        RETURN
-    END
+100       PAUSE 'Open params file error!'
+          STOP
 
+101       PAUSE 'Read params file error!'
+          CLOSE(10)
+          STOP
 
-    SUBROUTINE WRITE_ROW(X, M, REAL_M)
-        IMPLICIT NONE
-        COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP          
-        REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
-        REAL X, Y, ARCCOS_RES
-        REAL ARCCOS
-        INTEGER M, I, J, IS_INVISIBLE, REAL_M
-        
-        WRITE(10, 11, ERR=101) '|'           ! Запись первого столбца  
-        WRITE(10, 10, ERR=101) X, '|'        
-                                            
-        Y = Y_MIN                            
-        ARCCOS_RES = ARCCOS(X, Y)            
-        IF(ARCCOS_RES.NE.0.0) THEN
-            WRITE(10, 10, ERR=101) ARCCOS_RES, '|'   
-        ELSE
-            WRITE(10, 11, ERR=101) '        N/D|'    
-        ENDIF
-        
-        DO J=1, M-2, 1                       ! Запись решений с 2 до M-1 столбца 
-            IF(Y + Y_STEP.GT.Y_MAX) THEN
-                EXIT                         
-            ENDIF  
-            IF(IS_INVISIBLE(Y, Y + Y_STEP).EQ.1) THEN
-                Y = Y + Y_STEP               
-                CYCLE                    
-            ENDIF
-            Y = Y + Y_STEP                    
-            IF(ABS(Y).LT.ABS(Y_STEP/2)) THEN
-                Y = 0                       
-            ENDIF
-            ARCCOS_RES = ARCCOS(X, Y)          
+102       PAUSE 'Incorrect data, max must be greater than min!'
+          STOP
 
-            IF(ARCCOS_RES.NE.0.0) THEN
-                WRITE(10, 10, ERR=101) ARCCOS_RES, '|'  
-            ELSE
-                WRITE(10, 11, ERR=101) '        N/D|' 
-            ENDIF
-        ENDDO
-        
-        ! Запись последнего столбца
-        IF(IS_INVISIBLE(Y, Y_MAX).EQ.0) THEN   
-        ARCCOS_RES = ARCCOS(X, Y_MAX)     
-            IF(ARCCOS_RES.NE.0.0) THEN
-                WRITE(10, 10, ERR=101) ARCCOS_RES, '|'
-            ELSE
-                WRITE(10, 11, ERR=101) '        N/D|'
-            ENDIF
-        ENDIF
-            
-        WRITE(10, 12, ERR=101)               
-        
-        DO J=1, REAL_M, 1
-            WRITE(10, 11, ERR=101) '------------'     
-        ENDDO
-        WRITE (10, 11, ERR=101) '-------------'       
-        WRITE(10, 12, ERR=101)
-        
-        GOTO 109
-        
-    10        FORMAT(E11.4, A, $)                  ! Формат записи значений
-    11        FORMAT(A, $)                         ! Формат для N/D
-    12        FORMAT()                             ! Формат endl
+103       PAUSE 'Incorrect data, step must be greater than 0!'
+          STOP
 
-    101       PAUSE 'Write table file error!'
-        CLOSE(10)   
-        STOP
-        
-    109       CONTINUE 
-    END
+109       CONTINUE
+      END
 
+      REAL FUNCTION TO_RAD(ANGLE)
+          IMPLICIT NONE
+          REAL ANGLE
+          TO_RAD = ANGLE / 180.0 * 3.1415926
+      END
 
-        
-    SUBROUTINE CREATE_TABLE()
-        IMPLICIT NONE
-        COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
-        REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP, X, Y
-        INTEGER N, M, I, J, REAL_M, IS_INVISIBLE
-        
-        REAL_M = 1
+      INTEGER FUNCTION IS_INVISIBLE(FIRST, SECOND)
+          IMPLICIT NONE
+          REAL FIRST, SECOND
+          REAL EPSILON
+          PARAMETER (EPSILON = 1.0E-6)
 
-        ! Установка минимальных значений как начальных
-        X = X_MIN    
-        Y = Y_MIN
-        
-        ! Расчет размера матрицы (кол-во шагов)
-        N = (X_MAX - X_MIN) / X_STEP + 1
-        M = (Y_MAX - Y_MIN) / Y_STEP + 1
-        
-        ! Последние элементы не точно делятся - добавляем
-        IF((X_MIN + (N - 1) * X_STEP).NE.X_MAX) THEN
-            N = N + 1 
-        ENDIF
-        
-        IF((Y_MIN + (M - 1) * Y_STEP).NE.Y_MAX) THEN
-            M = M + 1
-        ENDIF
-        
-        ! открываем таблицу
-        OPEN(10, FILE='table.txt', ERR=100)
-                                                    
-        WRITE(10, 11, ERR=101) '|        X/Y|'         
-        WRITE(10, 10, ERR=101) Y_MIN, '|'               
-        DO I=1, M-2, 1                                  
-            IF(IS_INVISIBLE(Y, Y + Y_STEP).EQ.1) THEN
-                Y = Y + Y_STEP                         
-                CYCLE                                  
-            ENDIF
-            Y = Y + Y_STEP
-            IF(ABS(Y).LT.ABS(Y_STEP/2)) THEN
-                Y = 0             
-            ENDIF
-            WRITE(10, 10, ERR=101) Y, '|'   
-            REAL_M = REAL_M + 1                           
-        ENDDO
-        IF(IS_INVISIBLE(Y, Y_MAX).EQ.0) THEN
-            REAL_M = REAL_M + 1
-            WRITE(10, 10, ERR=101) Y_MAX, '|'
-        ENDIF
-        WRITE(10, 12, ERR=101)
-        DO J=1, REAL_M, 1
-            WRITE(10, 11, ERR=101) '------------'
-        ENDDO
-        WRITE (10, 11, ERR=101) '-------------'
-        WRITE(10, 12, ERR=101)       
-        
-        CALL WRITE_ROW(X_MIN, M, REAL_M)  
-        
-        
-        !Write from 2 to N-1 row           
-        DO I=1, N-2, 1          
-            IF(IS_INVISIBLE(X, X + X_STEP).EQ.0) THEN
-                X = X + X_STEP             
-                IF(ABS(X).LT.ABS(X_STEP/2)) THEN
-                    X = 0                  
-                ENDIF    
-                CALL WRITE_ROW(X, M, REAL_M) 
-            ELSE
-                X = X + X_STEP              
-            ENDIF
-        ENDDO
-        
-        !Заполнение последней строки
-        IF(IS_INVISIBLE(X, X_MAX).EQ.0) THEN
-            CALL WRITE_ROW(X_MAX, M, REAL_M) 
-        ENDIF
-        
-        CLOSE(10)
-        
-        GOTO 109
-        
-    10        FORMAT(E11.4, A, $)  
-    11        FORMAT(A, $)         
-    12        FORMAT()             
+          IF (ABS(FIRST - SECOND) .LT. EPSILON) THEN
+              IS_INVISIBLE = 1
+          ELSE
+              IS_INVISIBLE = 0
+          END IF
+      END
 
-    100       PAUSE 'Open table file error!'
-        STOP   
+      REAL FUNCTION ARCCOS(SUM)
+          IMPLICIT NONE
+          REAL SUM
+          REAL EPSILON
+          PARAMETER (EPSILON = 1.0E-6)
 
-    101       PAUSE 'Write table file error!'
-        CLOSE(10)   
-        STOP
-        
-    109       CONTINUE   
-    END
+          IF (SUM .LT. -1.0 .OR. SUM .GT. 1.0) THEN
+              ARCCOS = -1.0
+          ELSE IF (ABS(SUM - 1.0) .LT. EPSILON) THEN
+              ARCCOS = 0.0
+          ELSE
+              ARCCOS = ACOS(SUM)
+          END IF
+      END
+
+      SUBROUTINE WRITE_ROW(X, M, REAL_M)
+          IMPLICIT NONE
+          COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
+          REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
+          REAL X, Y, ARCCOS_RES, ARCCOS
+          INTEGER M, J, IS_INVISIBLE, REAL_M
+
+          WRITE(10, 11, ERR=101) '|'
+          WRITE(10, 10, ERR=101) X, '|'
+
+          Y = Y_MIN
+          ARCCOS_RES = ARCCOS(X + Y)
+          IF (ARCCOS_RES .GE. 0.0) THEN
+              WRITE(10, 10, ERR=101) ARCCOS_RES, '|'
+          ELSE
+              WRITE(10, 11, ERR=101) '        N/D|'
+          END IF
+
+          DO J = 1, M - 2, 1
+              IF (Y + Y_STEP .GT. Y_MAX) THEN
+                  EXIT
+              END IF
+              IF (IS_INVISIBLE(Y, Y + Y_STEP) .EQ. 1) THEN
+                  Y = Y + Y_STEP
+                  CYCLE
+              END IF
+              Y = Y + Y_STEP
+              IF (ABS(Y) .LT. ABS(Y_STEP / 2)) THEN
+                  Y = 0.0
+              END IF
+              ARCCOS_RES = ARCCOS(X + Y)
+              IF (ARCCOS_RES .GE. 0.0) THEN
+                  WRITE(10, 10, ERR=101) ARCCOS_RES, '|'
+              ELSE
+                  WRITE(10, 11, ERR=101) '        N/D|'
+              END IF
+          END DO
+
+          IF (IS_INVISIBLE(Y, Y_MAX) .EQ. 0) THEN
+              ARCCOS_RES = ARCCOS(X + Y_MAX)
+              IF (ARCCOS_RES .GE. 0.0) THEN
+                  WRITE(10, 10, ERR=101) ARCCOS_RES, '|'
+              ELSE
+                  WRITE(10, 11, ERR=101) '        N/D|'
+              END IF
+          END IF
+
+          WRITE(10, 12, ERR=101)
+
+          DO J = 1, REAL_M, 1
+              WRITE(10, 11, ERR=101) '------------'
+          END DO
+          WRITE(10, 11, ERR=101) '-------------'
+          WRITE(10, 12, ERR=101)
+
+          GOTO 109
+
+10        FORMAT(E11.4, A, $)
+11        FORMAT(A, $)
+12        FORMAT()
+
+101       PAUSE 'Write table file error!'
+          CLOSE(10)
+          STOP
+
+109       CONTINUE
+      END
+
+      SUBROUTINE CREATE_TABLE()
+          IMPLICIT NONE
+          COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
+          REAL X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP, X, Y
+          INTEGER N, M, I, J, REAL_M, IS_INVISIBLE
+
+          REAL_M = 1
+
+          N = INT((X_MAX - X_MIN) / X_STEP) + 1
+          IF (X_MIN + (N - 1) * X_STEP .LT. X_MAX) THEN
+              N = N + 1
+          END IF
+
+          M = INT((Y_MAX - Y_MIN) / Y_STEP) + 1
+          IF (Y_MIN + (M - 1) * Y_STEP .LT. Y_MAX) THEN
+              M = M + 1
+          END IF
+
+          OPEN(10, FILE='table.txt', ERR=200)
+
+          WRITE(10, 21, ERR=201) '|        X/Y|'
+          WRITE(10, 20, ERR=201) Y_MIN, '|'
+          DO J = 1, M - 1, 1
+              Y = Y_MIN + J * Y_STEP
+              IF (Y .GT. Y_MAX) THEN
+                  Y = Y_MAX
+              END IF
+              IF (IS_INVISIBLE(Y, Y + Y_STEP) .EQ. 1) THEN
+                  CYCLE
+              END IF
+              IF (ABS(Y) .LT. ABS(Y_STEP / 2)) THEN
+                  Y = 0.0
+              END IF
+              WRITE(10, 20, ERR=201) Y, '|'
+              REAL_M = REAL_M + 1
+          END DO
+          IF (IS_INVISIBLE(Y, Y_MAX) .EQ. 0) THEN
+              REAL_M = REAL_M + 1
+              WRITE(10, 20, ERR=201) Y_MAX, '|'
+          END IF
+          WRITE(10, 22, ERR=201)
+          DO J = 1, REAL_M, 1
+              WRITE(10, 21, ERR=201) '------------'
+          END DO
+          WRITE(10, 21, ERR=201) '-------------'
+          WRITE(10, 22, ERR=201)
+
+          DO I = 1, N, 1
+              X = X_MIN + (I - 1) * X_STEP
+              IF (X .GT. X_MAX) THEN
+                  X = X_MAX
+              END IF
+              IF (IS_INVISIBLE(X, X + X_STEP) .EQ. 0) THEN
+                  IF (ABS(X) .LT. ABS(X_STEP / 2)) THEN
+                      X = 0.0
+                  END IF
+                  CALL WRITE_ROW(X, M, REAL_M)
+              END IF
+          END DO
+
+          CLOSE(10)
+          GOTO 209
+
+20        FORMAT(E11.4, A, $)
+21        FORMAT(A, $)
+22        FORMAT()
+
+200       PAUSE 'Open table file error!'
+          STOP
+
+201       PAUSE 'Write table file error!'
+          CLOSE(10)
+          STOP
+
+209       CONTINUE
+      END
