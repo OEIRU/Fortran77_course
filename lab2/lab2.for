94018ae9fa0dafbf2cc6bfc3c8883d5a890d89bf
VKostovsky <86968295+OEIRU@users.noreply.github.com>
2025-03-05 11:48:36 +0700

    lab2: fix 62

--- lab2/lab2.for
+++ lab2/lab2.for
@@ -1,5 +1,3 @@
-! Версия с автоматическим выявлением 4 значащих цифр
-
       PROGRAM MAIN
       IMPLICIT NONE
       COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
@@ -18,12 +16,12 @@
       READ(10, *, ERR=100) X_STEP, Y_STEP
       CLOSE(10)
 
-      IF (ABS(X_STEP/X_MIN) .LT. 0.00009 .OR. 
+      IF (ABS(X_STEP/X_MIN) .LT. 0.00009 .OR.
      & ABS(Y_STEP/Y_MIN) .LT. 0.00009 ) THEN
         PRINT *, 'Error: Small step!'
         STOP
       END IF
-      
+
       RETURN
 100   PRINT *, 'Error reading params file!'
       STOP
@@ -41,118 +39,127 @@
       END IF
       END
 
-      SUBROUTINE GET_FORMAT(NUMBER, FMT)
-      IMPLICIT NONE
-      REAL*8 NUMBER
-      CHARACTER*20 FMT
-
-      ! Используем экспоненциальный формат с 4 значащими цифрами
-      WRITE(FMT, '(A)') '(ES12.3)'
-      END
-
-      SUBROUTINE NUMBER_TO_STRING(NUMBER, FMT, STR)
-      IMPLICIT NONE
-      REAL*8 NUMBER
-      CHARACTER*20 FMT, STR
-      WRITE(STR, FMT) NUMBER
-      END
-
       SUBROUTINE WRITE_ROW(CUR_X, Y_VALS, M)
       IMPLICIT NONE
       REAL*8 CUR_X, Y_VALS(*), ARCCOS, RES
       INTEGER M, J
-      CHARACTER*20 FMT, STR
+      CHARACTER*11 RES_STR
       EXTERNAL ARCCOS
 
-      CALL GET_FORMAT(CUR_X, FMT)
-      CALL NUMBER_TO_STRING(CUR_X, FMT, STR)
-      WRITE(10, '(A, A, A, $)') '| ', STR, ' '
+      ! Запись заголовка строки с текущим X
+      CALL FORMAT_NUMBER(CUR_X, RES_STR)
+      WRITE(10, '(A, A, A, $)') '| ', RES_STR, ' '
 
-      DO J = 1, M
-        RES = ARCCOS(CUR_X + Y_VALS(J))
-        IF (RES .EQ. -1.0D0) THEN
-          WRITE(10, '(A, A, A, $)') '| ', 'N/D                 ', ' '
-        ELSE
-          CALL GET_FORMAT(RES, FMT)
-          CALL NUMBER_TO_STRING(RES, FMT, STR)
-          WRITE(10, '(A, A, A, $)') '| ', STR, ' '
-        END IF
-      END DO
-
+      DO 10 J = 1, M
+          RES = ARCCOS(CUR_X + Y_VALS(J))
+          IF (RES .EQ. -1.0D0) THEN
+              RES_STR = 'N/D       '  ! Специальное значение для ошибок
+          ELSE
+              CALL FORMAT_NUMBER(RES, RES_STR)
+          END IF
+          WRITE(10, '(A, A, A, $)') '| ', RES_STR, ' '
+10    CONTINUE
       WRITE(10, '(A)') '|'
+      END
+
+      SUBROUTINE FORMAT_NUMBER(NUM, STR)
+      IMPLICIT NONE
+      REAL*8 NUM
+      CHARACTER*11 STR
+      CHARACTER*11 TEMP_STR
+
+      ! Записываем число в экспоненциальном формате E10.4
+      WRITE(TEMP_STR, '(E10.4)') NUM
+
+      ! Проверяем, есть ли ведущий ноль перед десятичной точкой
+      IF (TEMP_STR(1:1) .EQ. '-' .AND. TEMP_STR(2:2) .EQ. '.') THEN
+          ! Если число отрицательное и нет ведущего нуля, добавляем его
+          STR = '-0' // TEMP_STR(2:10)
+      ELSE IF (TEMP_STR(1:1) .EQ. ' ' .AND. TEMP_STR(2:2) .EQ. '.') THEN
+          ! Если число положительное и нет ведущего нуля, добавляем его
+          STR = ' 0' // TEMP_STR(2:10)
+      ELSE
+          ! В остальных случаях оставляем строку без изменений
+          STR = TEMP_STR
+      END IF
       END
 
       SUBROUTINE CREATE_TABLE()
       IMPLICIT NONE
       COMMON /INPUT/ X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
       REAL*8 X_MAX, Y_MAX, X_MIN, Y_MIN, X_STEP, Y_STEP
-      REAL*8 CUR_X, Y_VALS(1000)
+      REAL*8 CUR_X, Y_VALS(2000)  ! Увеличиваем размер массива
       INTEGER N, M, I, J
       LOGICAL HAS_VALID_VALUE
-      CHARACTER*20 FMT, STR
+      CHARACTER*11 RES_STR
 
+      ! Проверка шага
       IF (X_STEP .LE. 0.0D0 .OR. Y_STEP .LE. 0.0D0) THEN
         PRINT *, 'Error: Step must be positive!'
         STOP
       END IF
 
+      ! Вычисление размеров таблицы
       N = INT((X_MAX - X_MIN) / X_STEP) + 1
       IF (X_MIN + (N - 1) * X_STEP .LT. X_MAX) N = N + 1
-
       M = INT((Y_MAX - Y_MIN) / Y_STEP) + 1
       IF (Y_MIN + (M - 1) * Y_STEP .LT. Y_MAX) M = M + 1
 
-      IF (M .GT. 1000) THEN
+      IF (M .GT. 2000) THEN
         PRINT *, 'Error: Y_VALS array size exceeded!'
         STOP
       END IF
 
+      ! Проверка наличия допустимых значений
       HAS_VALID_VALUE = .FALSE.
-      DO I = 1, N
-        DO J = 1, M
-          IF (ABS(X_MIN + (I - 1) * X_STEP + Y_MIN +
+      DO 20 I = 1, N
+          DO 20 J = 1, M
+              IF (ABS(X_MIN + (I - 1) * X_STEP + Y_MIN +
      & (J - 1) * Y_STEP) .LE. 1.0D0) THEN
-            HAS_VALID_VALUE = .TRUE.
-            GOTO 100
-          END IF
-        END DO
-      END DO
-100   CONTINUE
-
+                  HAS_VALID_VALUE = .TRUE.
+                  GOTO 20
+              END IF
+20    CONTINUE
       IF (.NOT. HAS_VALID_VALUE) THEN
         PRINT *, 'Error: No valid values in the specified range!'
         STOP
       END IF
 
-      DO J = 1, M
-        Y_VALS(J) = Y_MIN + (J - 1) * Y_STEP
-      END DO
+      ! Заполнение массива Y_VALS
+      DO 50 J = 1, M
+          Y_VALS(J) = Y_MIN + (J - 1) * Y_STEP
+50    CONTINUE
 
+      ! Открытие файла для записи таблицы
       OPEN(10, FILE='table.txt', STATUS='UNKNOWN', ERR=200)
-      WRITE(10, '(A, $)') '|       X/Y            '
-      DO J = 1, M
-        CALL GET_FORMAT(Y_VALS(J), FMT)
-        CALL NUMBER_TO_STRING(Y_VALS(J), FMT, STR)
-        WRITE(10, '(A, A, A, $)') '| ', STR, ' '
-      END DO
+
+      ! Заголовок таблицы
+      WRITE(10, '(A, $)') '|      X/Y    '
+      DO 60 J = 1, M
+          CALL FORMAT_NUMBER(Y_VALS(J), RES_STR)
+          WRITE(10, '(A, A, A, $)') '| ', RES_STR, ' '
+60    CONTINUE
       WRITE(10, '(A)') '|'
 
-      WRITE(10, '(A, $)') '+----------------------'
-      DO J = 1, M
-        WRITE(10, '(A, $)') '+----------------------'
-      END DO
+      ! Линия разделителя
+      WRITE(10, '(A, $)') '+-------------'
+      DO 70 J = 1, M
+          WRITE(10, '(A, $)') '+-------------'
+70    CONTINUE
       WRITE(10, '(A)') '+'
 
-      DO I = 1, N
-        CUR_X = X_MIN + (I - 1) * X_STEP
-        IF (I .EQ. N) CUR_X = X_MAX
-        CALL WRITE_ROW(CUR_X, Y_VALS, M)
-        WRITE(10, '(A, $)') '+----------------------'
-        DO J = 1, M
-          WRITE(10, '(A, $)') '+----------------------'
-        END DO
-        WRITE(10, '(A)') '+'
-      END DO
+      ! Запись строк таблицы
+      DO 80 I = 1, N
+          CUR_X = X_MIN + (I - 1) * X_STEP
+          IF (I .EQ. N) CUR_X = X_MAX
+          CALL WRITE_ROW(CUR_X, Y_VALS, M)
+
+          WRITE(10, '(A, $)') '+-------------'
+          DO 90 J = 1, M
+              WRITE(10, '(A, $)') '+-------------'
+90        CONTINUE
+          WRITE(10, '(A)') '+'
+80    CONTINUE
 
       CLOSE(10)
       RETURN
